97c8f870c29e5334e120c79be4327262b9c64925
patilranasinha-git <patilranasinha@gmail.com>
2026-07-23 18:01:44 +0530

    feat: initialize project dependencies and scaffold base extension structure

--- extension/extension.ts
+++ extension/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { getGitHubSession } from './github';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('GIT-Alpha is now active!');
@@ -8,7 +9,11 @@ export function activate(context: vscode.ExtensionContext) {
         // Phase 2 will implement the webview panel here
     });
 
-    context.subscriptions.push(disposable);
+    let loginDisposable = vscode.commands.registerCommand('git-alpha.login', async () => {
+        await getGitHubSession();
+    });
+
+    context.subscriptions.push(disposable, loginDisposable);
 }
 
 export function deactivate() {}
